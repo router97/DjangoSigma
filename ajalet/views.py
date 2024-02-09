cf5c9467e20c0d89653ce9de18b2ed47a2946a9f
router <you@example.com>
2024-02-09 21:43:51 +0300

    commit

---
+++ ajalet/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Ajalet welcome to my first java project etot sayt tyt sigma odna storinka heelo world print print('Hello world')")
+
